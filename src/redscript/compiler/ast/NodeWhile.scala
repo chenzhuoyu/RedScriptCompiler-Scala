e6585a0b4fbbbb52a6f6ee77adcde437c776aaa3
Oxygen <chenzhuoyu1992@gmail.com>
2016-02-12 19:08:12 +0800

    while 循环支持 break/continue

--- src/redscript/compiler/ast/NodeWhile.scala
+++ src/redscript/compiler/ast/NodeWhile.scala
@@ -11,11 +11,14 @@ class NodeWhile(cond: NodeExpr, body: List[NodeStatement]) extends Node
         val endwhile = new Label
 
         assembler.visitor.visitLabel(label)
+        assembler.classes.top.method.enterLoop()
         cond.assemble(assembler)
         assembler.visitor.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "redscript/lang/RedObject", "__bool__", "()Z", false)
         assembler.visitor.visitJumpInsn(Opcodes.IFEQ, endwhile)
         body.foreach(_.assemble(assembler))
+        assembler.classes.top.method.patchContinues()
         assembler.visitor.visitJumpInsn(Opcodes.GOTO, label)
         assembler.visitor.visitLabel(endwhile)
+        assembler.classes.top.method.patchBreaks()
     }
 }
